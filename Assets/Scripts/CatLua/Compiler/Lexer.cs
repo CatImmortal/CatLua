d46614cb1101af56f912999181fb1bc67eface2b
CatImmortal <1304085906@qq.com>
2021-06-25 00:06:00 +0800

    增加词法分析器相关代码

---
+++ Assets/Scripts/CatLua/Compiler/Lexer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CatLua
+{
+    /// <summary>
+    /// 词法分析器
+    /// </summary>
+    public class Lexer
+    {
+        public Lexer(string chunk, string chunkName)
+        {
+            Chunk = chunk;
+            ChunkName = chunkName;
+            Line = 1;
+        }
+
+        public static Dictionary<string, TokenType> TokenMap = new Dictionary<string, TokenType>
+        {
+            { "and",TokenType.OpAnd},
+            { "break",TokenType.KwBreak},
+            { "do",TokenType.KwDo},
+            { "else",TokenType.KwElse},
+            { "elseif",TokenType.KwElseif},
+            { "end",TokenType.KwEnd},
+            { "false",TokenType.KwFalse},
+            { "for",TokenType.KwFor},
+            { "function",TokenType.KwFunction},
+            { "goto",TokenType.KwGoto},
+            { "if",TokenType.KwIf},
+            { "in",TokenType.KwIn},
+            { "local",TokenType.KwLocal},
+            { "nil",TokenType.KwNil},
+            { "not",TokenType.OpNot},
+            { "or",TokenType.OpOr},
+            { "repeat",TokenType.KwRepeat},
+            { "return",TokenType.KwReturn},
+            { "then",TokenType.KwThen},
+            { "true",TokenType.KwTrue},
+            { "until",TokenType.KwUntil},
+            { "while",TokenType.KwWhile},
+        };
+
+        /// <summary>
+        /// 源代码
+        /// </summary>
+        public string Chunk;
+
+        /// <summary>
+        /// 源文件名
+        /// </summary>
+        public string ChunkName;
+
+        /// <summary>
+        /// 当前行号
+        /// </summary>
+        public int Line;
+
+      
+
+        /// <summary>
+        /// 返回下一个token（会跳过空白字符和注释）
+        /// </summary>
+        public void NextToken(int kind,int line,string token)
+        {
+
+        }
+    }
+}
+
---
+++ Assets/Scripts/CatLua/Compiler/TokenType.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CatLua
+{
+    /// <summary>
+    /// Token类型
+    /// </summary>
+    public enum TokenType
+    {
+        Eof,  //end-of-file
+        Vararg,  //...
+
+        SepSemi,  //;
+        SepComma,  //,
+        SepDot,  //.
+        SepColon,  //:
+        SepLable,  //::
+        SepLparen,  //(
+        SepRparen,  //)
+        SepLbrack,  //[
+        SepRbrack,  //]
+        SepLcurly,  //{
+        SepRcurly,  //}
+
+
+        OpAsssign,  //=
+        OpMinus,  //-
+        OpWave,  //~
+        OpAdd,  //+
+        OpMul,  //*
+        OpDiv,  // /
+        OpIDiv,  // //
+        OpPow,  //^
+        OpMod,  //%
+        OpBAnd,  //&
+        OpBOr,  //|
+        OpShR,  //>>
+        OpShL,  //<<
+        OpConcat,  //..
+        OpLt,  //<
+        OpLe,  //<=
+        OpGt,  //>
+        OpGe,  //>=
+        OpEq,  //==
+        OpNe,  //~=
+        OpLen,  //#
+        OpAnd,  //and
+        OpOr,  //or
+        OpNot,  //not
+
+
+        KwBreak,  //break
+        KwDo,  //do
+        KwElse,  //else
+        KwElseif,  //elseif
+        KwEnd,  //end
+        KwFalse,  //false
+        KwFor,  //for
+        KwFunction,  //function
+        KwGoto,  //goto
+        KwIf,  //if
+        KwIn,  //in
+        KwLocal,  //local
+        KwNil,  //nil
+        KwRepeat,  //repeat
+        KwReturn,  //return
+        KwThen,  //then
+        KwTrue,  //true
+        KwUntil,  //until
+        KwWhile,  //while
+
+        Identifier,  //identifier
+        Number,  //number literal
+        String,   //string literal
+
+        OpUnm = OpMinus,
+        OpSub = OpMinus,
+        OpBNot = OpWave,
+        OpBXor = OpWave,
+    }
+}
+
+
